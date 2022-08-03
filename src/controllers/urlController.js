825f4a5eafd44f4615ddd30e904ab46cadb6f9dc
Isadora Ávila <isadorgravila@gmail.com>
2022-08-03 13:24:39 -0300

    Rota de pegar url pelo id: Verifica se o url existe em urls e se sim, responde com o url

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,10 +5,26 @@ export async function shortenUrl(req, res) {
     const { url } = req.body;
     const userId = res.locals.userId;
     const shortUrl = nanoid();
-
     try {
         await connection.query(`INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3)`, [userId, url, shortUrl]);
         return res.sendStatus(201);
+
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+export async function getUrlById(req, res) {
+    const { id } = req.params;
+    try {
+        const { rows } = await connection.query(`SELECT id, "shortUrl", url FROM urls WHERE id = $1`, [id]);
+        const url = rows[0];
+
+        if (!url) {
+            return res.sendStatus(404);
+        }
+
+        return res.status(200).send(url);
 
     } catch (error) {
         return res.status(500).send(error);
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { shortenUrl } from '../controllers/urlController.js';
+import { shortenUrl, getUrlById } from '../controllers/urlController.js';
 import urlValidation from '../middlewares/urlValidation.js';
 import tokenValidation from '../middlewares/tokenValidation.js';
 
 const router = Router();
 
 router.post('/urls/shorten', tokenValidation, urlValidation, shortenUrl);
+router.get('/urls/:id', getUrlById);
 
 export default router;
