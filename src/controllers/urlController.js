161c62a0f324bb1cc396e0cfdfdb589222a9bc9f
Isadora Ávila <isadorgravila@gmail.com>
2022-08-03 00:31:25 -0300

    Rota de encurtar URL: encurta url com nanoid e insere em urls

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,10 +1,13 @@
 import connection from '../databases/postgres.js';
+import { nanoid } from 'nanoid';
 
 export async function shortenUrl(req, res) {
+    const { url } = req.body;
     const userId = res.locals.userId;
-    try {
-        console.log(userId);
+    const shortUrl = nanoid();
 
+    try {
+        await connection.query(`INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3)`, [userId, url, shortUrl]);
         return res.sendStatus(201);
 
     } catch (error) {
