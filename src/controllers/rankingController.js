bec5badb5fde77508f5eb7c5955b00a6e55897f6
Isadora Ávila <isadorgravila@gmail.com>
2022-08-04 00:06:35 -0300

    Rota de ranking: correção da query para ordenação

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -3,19 +3,18 @@ import connection from '../databases/postgres.js';
 export async function getRanking(req, res) {
     try {
         const { rows: ranking } = await connection.query(`
-        SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls."visitCount") AS "visitCount" 
+        SELECT 
+            users.id AS id, 
+            users.name AS name, 
+            CAST(COUNT(urls.url) AS INTEGER) AS "linksCount", 
+            COALESCE(CAST(SUM(urls."visitCount") AS INTEGER), 0) AS "visitCount" 
         FROM users 
         LEFT JOIN urls 
         ON urls."userId" = users.id 
         GROUP BY users.id, users.name 
-        ORDER BY "visitCount" DESC
+        ORDER BY "visitCount" DESC, "linksCount" DESC
         LIMIT 10
         `);
-
-        ranking.map(item => {
-            item.linksCount = Number(item.linksCount);
-            item.visitCount = Number(item.visitCount);
-        });
 
         return res.status(200).send(ranking);
 
